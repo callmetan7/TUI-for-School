a497c4a2fc9e3b8a411c130567f70fcfcb46ad46
callmetan <87515235+callmetan7@users.noreply.github.com>
2021-10-12 20:48:23 +0800

    Delete colors.py

--- colors.py
+++
@@ -1,8 +0,0 @@
-# This is the file that will simplify the syntax highlighting feature
-colors = {
-    # All the colors are written in the octa version ANSII escape code 
-    "white":  "\033[1;37;40m",
-    "cyan": "\033[1;49;96m",
-    "purple": "\033[1;49;95m",
-    "green": "\033[1;32;40m",
-}
